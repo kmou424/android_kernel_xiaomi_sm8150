ae880dac6766c76b6978dcf7b4fb74d1013e53a7
Julian Liu <wlootlxt123@gmail.com>
2019-08-10 21:16:54 +0800

    sched/tune: Get schedtune of top-app during initing css
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -21,7 +21,6 @@ extern struct reciprocal_value schedtune_spc_rdiv;
 #define DYNAMIC_BOOST_SLOTS_COUNT 5
 static DEFINE_MUTEX(boost_slot_mutex);
 static DEFINE_MUTEX(stune_boost_mutex);
-static struct schedtune *getSchedtune(char *st_name);
 static int dynamic_boost(int boost);
 struct boost_slot {
 	struct list_head list;
@@ -818,6 +817,14 @@ schedtune_boostgroup_init(struct schedtune *st)
 	return 0;
 }
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+static void filterSchedtune(struct schedtune *sti, struct schedtune **sto_p, char *st_name)
+{
+	if (!strncmp(sti->css.cgroup->kn->name, st_name, strlen(st_name)))
+		*sto_p = sti;
+}
+#endif
+
 static struct cgroup_subsys_state *
 schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 {
@@ -837,6 +844,10 @@ schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx)
 		if (!allocated_group[idx])
 			break;
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+		filterSchedtune(allocated_group[idx], &st_ta, "top-app");
+#endif
+	}
 	if (idx == BOOSTGROUPS_COUNT) {
 		pr_err("Trying to create more than %d SchedTune boosting groups\n",
 		       BOOSTGROUPS_COUNT);
@@ -914,27 +925,6 @@ schedtune_init_cgroups(void)
 }
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static struct schedtune *getSchedtune(char *st_name)
-{
-	int idx;
-
-	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx) {
-		char name_buf[NAME_MAX + 1];
-		struct schedtune *st = allocated_group[idx];
-
-		if (!st) {
-			pr_warn("SCHEDTUNE: Could not find %s\n", st_name);
-			break;
-		}
-
-		cgroup_name(st->css.cgroup, name_buf, sizeof(name_buf));
-		if (strncmp(name_buf, st_name, strlen(st_name)) == 0)
-			return st;
-	}
-
-	return NULL;
-}
-
 static int dynamic_boost(int boost)
 {
 	int ret;
@@ -1110,7 +1100,6 @@ int reset_stune_boost(int slot)
 
 int do_stune_sched_boost(int *slot)
 {
-	st_ta = getSchedtune("top-app");
 	if (!st_ta)
 		return -EINVAL;
 
@@ -1119,7 +1108,6 @@ int do_stune_sched_boost(int *slot)
 
 int do_stune_boost(int boost, int *slot)
 {
-	st_ta = getSchedtune("top-app");
 	if (!st_ta)
 		return -EINVAL;
 
@@ -1128,7 +1116,6 @@ int do_stune_boost(int boost, int *slot)
 
 int get_sched_boost(void)
 {
-	st_ta = getSchedtune("top-app");
 	if (!st_ta)
 		return -EINVAL;
 
