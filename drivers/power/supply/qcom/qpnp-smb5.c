2fc00ff57813cbd0cf87b015d734040388666c4f
Julian Liu <wlootlxt123@gmail.com>
2020-03-01 10:09:45 +0800

    power: supply: Adjust charging thermal mitigation
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -543,7 +543,7 @@ static int smb5_parse_dt(struct smb5 *chip)
 			return rc;
 		}
 	}
-#ifdef CONFIG_THERMAL
+
 	if (of_find_property(node, "qcom,thermal-mitigation-dcp", &byte_len)) {
 		chg->thermal_mitigation_dcp = devm_kzalloc(chg->dev, byte_len,
 			GFP_KERNEL);
@@ -790,7 +790,7 @@ static int smb5_parse_dt(struct smb5 *chip)
 			return rc;
 		}
 	}
-#else
+
 	if (of_find_property(node, "qcom,thermal-mitigation", &byte_len)) {
 		chg->thermal_mitigation = devm_kzalloc(chg->dev, byte_len,
 			GFP_KERNEL);
@@ -809,7 +809,6 @@ static int smb5_parse_dt(struct smb5 *chip)
 			return rc;
 		}
 	}
-#endif
 
 	rc = of_property_read_u32(node, "qcom,charger-temp-max",
 			&chg->charger_temp_max);
