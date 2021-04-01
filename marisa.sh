be700a8998de656b6ef06254cb8d1d84ca53b636
kmou424 <me@kmou424.moe>
2021-04-01 07:03:37 +0000

    0.3.2-Q

--- marisa.sh
+++ marisa.sh
@@ -1,4 +1,4 @@
-VERSION="MarisaKernel-0.2.1-Q"
+VERSION="MarisaKernel-0.3.2-Q"
 
 sed -i "s/-MarisaKernel-perf/-"$VERSION"/g" arch/arm64/configs/raphael_defconfig
 sed -i "s/-MarisaKernel-perf/-"$VERSION"/g" arch/arm64/configs/cepheus_defconfig
