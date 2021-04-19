a07655747f2d96bb420185675e178c8571cf8cec
kmou424 <me@kmou424.moe>
2021-04-19 18:31:01 +0800

    marisa: Add commit hash to version support

--- marisa.sh
+++ marisa.sh
@@ -1,4 +1,11 @@
-VERSION="MarisaKernel-0.6.7-R"
+VERSION_NUMBER=0.6.7
+
+if [ -z "$KERNEL_RELESE" ]
+then
+VERSION="MarisaKernel-$VERSION_NUMBER-$LAST_COMMIT-R"
+else
+VERSION="MarisaKernel-$VERSION_NUMBER-R"
+fi
 
 sed -i "s/-MarisaKernel-perf/-"$VERSION"/g" arch/arm64/configs/raphael_defconfig
 sed -i "s/-MarisaKernel-perf/-"$VERSION"/g" arch/arm64/configs/cepheus_defconfig
